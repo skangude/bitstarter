6ee487607425aff05f102d1a30a96e3a7f9061c7
Shantanu Kangude <Shantanu@kangude.net>
2013-07-10 05:01:55 +0200

    reading from index.html and posting that output

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
 var express = require('express');
-
+var fs = require('fs');
 var app = express.createServer(express.logger());
 
+var out_str = fs.readFileSync('index.html');
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(out_str);
 });
 
 var port = process.env.PORT || 5000;
